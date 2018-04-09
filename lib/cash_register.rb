e11c7f47ede6202be3b0532fcedeebe76c843b87
Sara Ginsburg <sara.ginsburg.ny@gmail.com>
2018-04-09 00:06:53 +0000

    automatically backed up by learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -19,7 +19,6 @@ class CashRegister
   end
 
   def apply_discount
-
     if @discount > 0
       @total = @total * (100 - @discount ) / 100
       "After the discount, the total comes to $#{@total}."
